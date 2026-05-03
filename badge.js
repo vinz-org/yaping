2d14e49804627413686a57f27a0c0407802551ce
Faxy <goldye265@gmail.com>
2026-05-03 09:49:11 +0700

    update

---
+++ badge.js
@@ -0,0 +1,9 @@
+// ============================================
+// YAPING - Daftar Akun Resmi (Official Badge)
+// Tambahkan username di array di bawah ini
+// ============================================
+var YAPING_BADGE_USERS = [
+    '@vin',
+    '@peer-monsj9lz-5e38p82',
+    '@peer-mop5ghaf-vknt1li'
+];
