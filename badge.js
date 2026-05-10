7e49fe3e9d7402059a2703c8a33abccda43d43a5
Faxy <goldye265@gmail.com>
2026-05-10 15:42:43 +0700

    Update badge.js

--- badge.js
+++ badge.js
@@ -3,11 +3,9 @@
 // Tambahkan username di array di bawah ini
 // ============================================
 var YAPING_BADGE_USERS = [
-    '@vinnn',
+    '@vinnn'
     '@hexaa',
-    '@bit',
+    '@qwerty',
     'anotheroom',
     '@yaping',
-    '@peer-motz41b4-amjo9vy',
-    '@peer-mop5ghaf-vknt1li'
 ];
